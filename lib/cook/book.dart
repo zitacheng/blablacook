44af4cac70ae3c56f6dfdf2eec868ea7bd9d79fb
zitacheng <zita.cheng@epitech.eu>
2020-11-01 16:19:15 +0100

    booking choice done

--- lib/cook/book.dart
+++ lib/cook/book.dart
@@ -6,6 +6,8 @@ import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:parse_server_sdk/parse_server_sdk.dart';
 
+import '../utils.dart';
+
 class Book extends StatefulWidget {
   @override
   _BookState createState() => _BookState();
@@ -17,10 +19,21 @@ class _BookState extends State<Book> {
     final QueryBuilder<ParseObject> queryPost =
         QueryBuilder<ParseObject>(ParseObject('Book'))
           ..includeObject(<String>['client', 'cook'])
-          ..whereEqualTo('cook', currentUser)
-          ..whereEqualTo('accepted', null);
+          ..whereEqualTo('cook', currentUser);
     final ParseResponse apiResponse = await queryPost.query();
     return apiResponse;
+  }
+
+  // ignore: avoid_void_async
+  void choiceBooking(dynamic data, bool choice) async {
+    data.set('accepted', choice);
+    final dynamic response = await data.save();
+    if (response.success == true) {
+      showAlertDialog(context, 'Réussi', 'choix pris en compte');
+      setState(() {});
+    } else {
+      showAlertDialog(context, 'Erreur', 'veuillez réessayer');
+    }
   }
 
   @override
@@ -46,55 +59,120 @@ class _BookState extends State<Book> {
                       fontSize: 40,
                     )),
               ),
-              for (dynamic data in state.book.data)
-                Slidable(
-                  actionPane: const SlidableDrawerActionPane(),
-                  actionExtentRatio: 0.25,
-                  child: Container(
-                    color: Colors.white,
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.deepOrangeAccent,
-                        child: state.user.img != null
-                            ? ClipRRect(
-                                borderRadius: BorderRadius.circular(60.0),
-                                child: Image.network(
-                                  state.user.img.url as String,
-                                  fit: BoxFit.cover,
+              if (state.book != null)
+                for (dynamic data in state.book.data)
+                  if (data.get('accepted') == null)
+                    Slidable(
+                      actionPane: const SlidableDrawerActionPane(),
+                      actionExtentRatio: 0.25,
+                      child: Container(
+                        color: Colors.white,
+                        child: ListTile(
+                          leading: data.get('client').get('img') != null
+                              ? ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.network(
+                                      data.get('client').get('img').url
+                                          as String,
+                                      fit: BoxFit.cover,
+                                      width: 50,
+                                      height: 50),
+                                )
+                              : ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.asset(
+                                    'assets/images/noAvatar.png',
+                                    fit: BoxFit.cover,
+                                    width: 50,
+                                    height: 50,
+                                  ),
                                 ),
-                              )
-                            : Image.asset(
-                                'assets/images/logo.png',
-                                width: 170,
-                                height: 170,
-                              ),
-                        foregroundColor: Colors.white,
+                          title: Text(
+                              data.get('client').get('username') as String),
+                          subtitle: Text(DateFormat('yyyy-MM-dd – kk:mm')
+                              .format(data.get('cookDate') as DateTime)),
+                        ),
                       ),
-                      title: Text(data.get('client').get('username') as String),
-                      subtitle: Text(DateFormat('yyyy-MM-dd – kk:mm')
-                          .format(data.get('cookDate') as DateTime)),
+                      secondaryActions: <Widget>[
+                        IconSlideAction(
+                          caption: 'Accepter',
+                          color: Colors.greenAccent,
+                          icon: Icons.check,
+                          onTap: () {
+                            choiceBooking(data, true);
+                          },
+                        ),
+                        IconSlideAction(
+                          caption: 'Refuser',
+                          color: Colors.deepOrange,
+                          icon: Icons.close,
+                          onTap: () {
+                            choiceBooking(data, false);
+                          },
+                        ),
+                      ],
+                    )
+                  else
+                    Container(
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: <Widget>[
+                            if (data.get('client').get('img') != null)
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(25.0),
+                                child: Image.network(
+                                    data.get('client').get('img').url as String,
+                                    fit: BoxFit.cover,
+                                    width: 50,
+                                    height: 50),
+                              )
+                            else
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(25.0),
+                                child: Image.asset(
+                                  'assets/images/noAvatar.png',
+                                  fit: BoxFit.cover,
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Container(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Text>[
+                                      Text(data.get('client').get('username')
+                                          as String),
+                                      Text(DateFormat('yyyy-MM-dd – kk:mm')
+                                          .format(data.get('cookDate')
+                                              as DateTime)),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                                data.get('accepted') == true
+                                    ? 'Réservation accepté'
+                                    : 'Réservation refusé',
+                                style: TextStyle(
+                                  color: data.get('accepted') == true
+                                      ? Colors.green
+                                      : Colors.red,
+                                  fontFamily: 'Amatic',
+                                  fontSize: 22,
+                                ))
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  secondaryActions: <Widget>[
-                    IconSlideAction(
-                      caption: 'Accepter',
-                      color: Colors.greenAccent,
-                      icon: Icons.check,
-                      onTap: () => _showSnackBar(),
-                    ),
-                    IconSlideAction(
-                      caption: 'Refuser',
-                      color: Colors.deepOrange,
-                      icon: Icons.close,
-                      onTap: () => _showSnackBar(),
-                    ),
-                  ],
-                ),
             ]))),
           );
         });
   }
 }
-
-// ignore: camel_case_types
-class _showSnackBar {}
